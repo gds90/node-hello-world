11a57e85e7407e71e712fb475cd73a3f55c7d895
gds90 <you@example.com>
2024-05-21 16:01:11 +0200

    Aggiunto Bonus per stampare frasi ispirazionali in pagina

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const http = require("http");
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
 
+// Array di frasi ispirazionali
+const ispiration_quotes = [
+    "Il successo &egrave; la somma di piccoli sforzi ripetuti giorno dopo giorno.",
+    "Trasforma le tue ferite in saggezza.",
+    "La migliore preparazione per domani &egrave; fare il tuo meglio oggi.",
+    "Stai guardando le onde, ma ignori il mare."
+];
+
+// Funzione che recupera una frase random dall'array delle frasi ispirazionali
+function getRandomQuote() {
+    return ispiration_quotes[Math.floor(Math.random() * ispiration_quotes.length)];
+}
+
 const server = http.createServer((req, res) => {
     console.log(`${req.method} | ${req.url} effettuata`);
 
@@ -15,9 +28,16 @@ const server = http.createServer((req, res) => {
 
     if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(`<h1>${process.env.VAR_HELLO_WORLD}</h1>`);
+        res.end(`
+            <h1>${process.env.VAR_HELLO_WORLD}</h1>
+            <h1>${getRandomQuote()}</h1>
+        `);
         return;
     }
+
+    // Gestione delle richieste non trovate
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>404 - Page not found</h1>');
 })
 
 server.listen(port, host, () => {
